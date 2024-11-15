991f8b756b545f813205e47e7a7e30b6c047f73e
Hunainvohra <hunainvohra31@gmail.com>
2024-11-15 22:46:17 +0500

    made some changes for email

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -161,6 +161,9 @@ namespace TaskManagementSystem.Controllers
                     // Send an email with this link
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    Services.EmailSender.SendEmail(user.Email, "Confirm your email",
+                    $"Please confirm your email by clicking <a href=\"{callbackUrl}\">here</a>.");
+
                     await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "Home");
---
+++ Services/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+
+namespace TaskManagementSystem.Services
+{
+public class EmailSender
+{
+    public static void SendEmail(string toEmail, string subject, string body)
+    {
+        var smtp = new SmtpClient
+        {
+            Host = "smtp.gmail.com",
+            Port = 587,
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential("hunainvohra31@gmail.com", "National@123")
+        };
+
+        using (var message = new MailMessage("hunainvohra31@gmail.com", toEmail)
+        {
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true
+        })
+        {
+            smtp.Send(message);
+        }
+    }
+}
+}
